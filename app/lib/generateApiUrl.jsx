b097dc7aac53d199f063f996695dc878677f3069
Hash-Ezz <ezz.alhashedi@gmail.com>
2024-09-20 10:56:53 +0300

    fix: the delay error and udate the code

--- app/lib/generateApiUrl.jsx
+++ app/lib/generateApiUrl.jsx
@@ -1,7 +1,6 @@
-const PUBLIC_API_KEY = '7945a4965e711e4f191d5e4b9e0274ce';
 const baseUrl = 'https://api.themoviedb.org/3/';
+import '../../.env';
 
-// Function to generate a TMDB API URL
 const generateApiUrl = ({
   type = 'movie',
   category = 'popular',
@@ -11,34 +10,29 @@ const generateApiUrl = ({
   query,
   queryParams = {}
 }) => {
-  // Determine the endpoint based on the type and category
-  let endpoint;
+  // Determine the endpoint based on the category and type
+  const endpoints = {
+    trending: `${type}/trending/${period}`,
+    search: `search/${type}`,
+    popular: `${type}/popular`
+  };
 
-  if (category === 'trending') {
-    endpoint = `${type}/trending/${period}`;
-  } else if (category === 'search') {
-    endpoint = `search/${type}`;
-  } else if (category === 'popular') {
-    endpoint = `${type}/popular`;
-  } else if (type === 'movie' && year) {
-    endpoint = `movie/${year}/popular`;
-  } else if (type === 'tv' && year) {
-    endpoint = `tv/${year}/popular`;
-  } else {
-    endpoint = `${type}/popular`;
-  }
+  const endpoint = year
+    ? `${type}/${year}/popular`
+    : endpoints[category] || endpoints.popular;
 
-  // Add search query if applicable
-  const queryParamString = [
-    ...Object.keys(queryParams).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(queryParams[key])}`),
-    query ? `query=${encodeURIComponent(query)}` : '',
-    `api_key=${PUBLIC_API_KEY}`,
-    `language=${lang}`
-  ].filter(Boolean).join('&');
+  // Create query parameters
+  const params = new URLSearchParams({
+    api_key: PUBLIC_API_KEY,
+    language: lang,
+    ...(query && { query }), // Include query if it exists
+    ...queryParams
+  });
 
   // Return the full API URL
-  console.log(`${baseUrl}${endpoint}?${queryParamString}`);
-  return `${baseUrl}${endpoint}?${queryParamString}`;
+  const apiUrl = `${baseUrl}${endpoint}?${params.toString()}`;
+  console.log(apiUrl); // For debugging
+  return apiUrl;
 };
 
 export default generateApiUrl;
