803813ac23c59eb662c1b4feccf5c07b4e63d87c
Hash-Ezz <ezz.alhashedi@gmail.com>
2024-09-21 10:37:03 +0300

    fix: imag error and use nextjs Image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**', // Path pattern for TMDB images
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com', // Allow placeholder images
+        pathname: '/**', // Allow all paths
+      },
+    ],
+  },
+};
 
 export default nextConfig;
