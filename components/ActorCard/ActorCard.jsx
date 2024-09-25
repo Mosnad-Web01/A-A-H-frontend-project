1a4502c6f60209b7487e94e42eef869945fc6b9d
Abdulmajeed Alselmi <hh738hhh@gmail.com>
2024-09-25 04:53:00 +0300

    style : add style for single actor page and add some features

--- components/ActorCard/ActorCard.jsx
+++ components/ActorCard/ActorCard.jsx
@@ -1,23 +1,18 @@
-import Link from 'next/link';
-
-export default function ActorCard({ actor }) {
-  const actorImage = actor.profile_path
-    ? `https://image.tmdb.org/t/p/w500${actor.profile_path}`
-    : '/default-actor.jpg'; // Placeholder image for actors without a picture
-
+export default function ActorCard({ actor, onClick }) {
   return (
-    <div className="bg-gray-900 shadow-md rounded-lg p-4">    
-      <img
-        src={actorImage}
-        alt={actor.name}
-        className="w-full h-64 object-cover rounded-lg"
+    <div
+      className="bg-white shadow-md rounded-lg overflow-hidden transform transition duration-500 hover:scale-105 cursor-pointer"
+      onClick={onClick} // Handle card click
+    >
+      <img 
+        src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} 
+        alt={actor.name} 
+        className="w-full h-64 object-cover"
       />
-      <h2 className="mt-2 text-xl font-bold">{actor.name}</h2>
-      <p className="text-sm">Popularity: {actor.popularity.toFixed(1)}</p>
-      
-      <Link href={`/actors/${actor.id}`} className="mt-3 inline-block text-blue-500 hover:underline">
-        View Details
-      </Link>
+      <div className="p-4">
+        <h3 className="text-lg font-bold text-gray-800">{actor.name}</h3>
+        <p className="text-gray-600">Known for: {actor.known_for_department}</p>
+      </div>
     </div>
   );
 }
